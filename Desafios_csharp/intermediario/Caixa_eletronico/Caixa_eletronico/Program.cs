b9ce3cca26b50696bfd68d10d4a1fe4922b45510
Kaippy <kaippy.canijo@gmail.com>
2025-10-02 17:49:42 -0300

    atualização - caixa eletronico - nível intermediário

---
+++ Desafios_csharp/intermediario/Caixa_eletronico/Caixa_eletronico/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Caixa_eletronico;
+
+public static class Program
+{
+    public static void Main()
+    {
+        bool exe = true;
+        string entrada;
+        int valor;
+        int valorTemp;
+        List<int> saque = new List<int>();
+
+        int[] notas = new[]
+            {
+            100, 50, 20, 10, 5, 2, 1
+            };
+
+        while (exe)
+        {
+            Console.WriteLine("Quanto gostaria de sacar?");
+            entrada = Console.ReadLine();
+            if (int.TryParse(entrada, out valor) && valor > 0)
+            {
+                saque.Clear();
+                valorTemp = valor;
+
+                for (int i = 0; i < notas.Length; i++)
+                {
+                    while (valorTemp >= notas[i])
+                    {
+                        valorTemp -= notas[i];
+                        saque.Add(notas[i]);
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida");
+                exe = false;
+            }
+
+            foreach (int nota in saque)
+            {
+                Console.WriteLine(nota);
+            }
+        }
+
+    }
+
+}
